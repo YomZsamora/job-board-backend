9e3b390ef5f18e9e68e598e26baeb8bfb3b00d9c
yomZsamora <samaurah@gmail.com>
2022-05-13 17:37:42 +0300

    Mail setup for Mailtrap. Password reset validation.

--- app/Http/Controllers/Auth/PasswordResetController.php
+++ app/Http/Controllers/Auth/PasswordResetController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Mail\PasswordReset;
+use Illuminate\Support\Facades\Mail;
 
 class PasswordResetController extends Controller
 {
     public function userResetPassword(Request $request) {
 
         if(User::where('email', $request->email)->first()){
-            
+            // $user = User::where('email', $request->email)->first();
+            Mail::to('samora.yommie@moringaschool.com')->send(new PasswordReset());
+
+            return response()->json(['status' => 200, 'errorTitle' => 'Email Sent!', 'errorMessage' => 'You will receive an email with instructions for resetting your password. Click on the link to reset your password!' ]);
         }
         return response()->json(['status' => 422, 'errorTitle' => 'Email not found!', 'errorMessage' => 'Email does not exist in our records!' ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/reset_pass', function () {
+    return view('reset_password');
+});
