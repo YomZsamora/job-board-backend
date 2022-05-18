2cb095721c312f5d75db09e83455b6fbec7a14aa
yomZsamora <samaurah@gmail.com>
2022-05-18 18:28:24 +0300

    Done: Finalised students upload with validation checks.

--- app/Http/Controllers/Auth/PasswordResetController.php
+++ app/Http/Controllers/Auth/PasswordResetController.php
@@ -14,8 +14,8 @@ class PasswordResetController extends Controller
 
         if(User::where('email', $request->email)->first()){
             Mail::to('samora.yommie@moringaschool.com')->send(new PasswordReset());
-            return response()->json(['status' => 200, 'errorTitle' => 'Email Sent!', 'errorMessage' => 'You will receive an email with instructions for resetting your password. Click on the link to reset your password!' ]);
+            return response()->json(['status' => 200, 'errorMessage' => 'You will receive an email with instructions for resetting your password. Click on the link to reset your password!' ]);
         }
-        return response()->json(['status' => 422, 'errorTitle' => 'Email not found!', 'errorMessage' => 'Email does not exist in our records!' ]);
+        return response()->json(['status' => 422, 'errorMessage' => 'Email does not exist in our records!' ]);
     }
 }
--- app/Http/Controllers/FileUploads/StudentsUploadController.php
+++ app/Http/Controllers/FileUploads/StudentsUploadController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Students;
 use App\Models\Cohorts;
+use App\Http\Controllers\FileUploads\UploadProcessingController;
 use Illuminate\Support\Facades\Hash;
 
 class StudentsUploadController extends Controller
@@ -20,31 +21,45 @@ class StudentsUploadController extends Controller
             $report = fopen(base_path("storage/app/public/uploads/" . $fileName), "r");
             $dataRow = true;
 
-            try {
-                while (($data = fgetcsv($report, 4000, ",")) !== FALSE) {
-                    if (!$dataRow) {
-                        $cohortExists = Cohorts::where('cohort', $data['3'])->first();
-                        if ($cohortExists) {
-                            Students::create([
-                                "first_name" => $data['0'],
-                                "last_name" => $data['1'],
-                                "email" => $data['2'],
-                                "cohort" => $cohortExists->id,
-                                "track" => $data['4'],
-                                "role_id" => 4,
-                                'password' => Hash::make('password'), 
-                            ]);  
+            // Validate Headers
+            $checkHeaders = new UploadProcessingController;
+            $passedHeadersCheck = $checkHeaders->validateCsvHeaders($fileName);
+
+            if($passedHeadersCheck) {
+                // Get No of Rows
+                $rows = new UploadProcessingController;
+                $row_count = $rows->getRowsCount($fileName);                
+
+                try {
+                    while (($data = fgetcsv($report, 4000, ",")) !== FALSE) {
+                        if (!$dataRow) {
+                            $cohortExists = Cohorts::where('cohort', $data['3'])->first();
+                            if ($cohortExists) {
+                                Students::create([
+                                    "first_name" => $data['0'],
+                                    "last_name" => $data['1'],
+                                    "email" => $data['2'],
+                                    "cohort" => $cohortExists->id,
+                                    "track" => $data['4'],
+                                    "role_id" => 4,
+                                    'password' => Hash::make('Grad@moringa1234!'), 
+                                ]);  
+                            } else {
+                                return response()->json(['status' => 422, 'title' => 'Cohort Not Found!', 'message' => 'Cohort ' . $data['3'] . " could not be found! Please ensure a cohort has been added before bulk upload. Click below to create a cohort now.", 'buttonText' => 'Add ' . $data['3'] . ' Cohort' ]);
+                            }
                         }
+                        $dataRow = false;
                     }
-                    $dataRow = false;
+                    fclose($report);
+                } catch (\Illuminate\Database\QueryException $exception) {
+                    $errorInfo = $exception->errorInfo;
+                    return response()->json(['status' => 500, 'message' => $errorInfo[2]]);
                 }
-                return response()->json(['status' => 500, 'title' => 'Cohort Not Found!', 'message' => 'Cohort ' . $data['3'] . "Can't be found!"]);
-                fclose($report);
-            } catch (\Illuminate\Database\QueryException $exception) {
-                $errorInfo = $exception->errorInfo;
-                return response()->json(['status' => 500, 'errors' => $errorInfo, 'title' => 'Duplicate Detected!', 'message' => 'Students have successfully been added to records!']);
+                return response()->json(['status' => 200, 'message' => $row_count-1 . ' students have successfully been added to records!']);
+
+            } else {
+                return response()->json(['status' => 500, 'message' => 'Error Importing! Check that the CSV File is in the correct format!']);
             }
-            return response()->json(['status' => 200, 'title' => 'File Uploaded!', 'message' => 'Students have successfully been added to records!']);
         }
     }
 }
