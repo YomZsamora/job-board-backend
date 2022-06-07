525711f3984dc2e1bab34355686aa19045edbe83
yomZsamora <samaurah@gmail.com>
2022-06-07 20:01:11 +0300

    Moving getGraduates function to correct controller - GraduatesController.

--- app/Http/Controllers/Cohorts/CohortsController.php
+++ app/Http/Controllers/Cohorts/CohortsController.php
@@ -8,11 +8,13 @@ use App\Models\Students;
 
 class CohortsController extends Controller
 {
+    // Fetch All Cohorts
     function getCohorts(){
         $cohorts = Cohorts::orderBy('end_date', 'DESC')->get();
         return response()->json(['status' => 200, 'cohorts' => $cohorts]);
     }
 
+    // Fetch Graduates for a Specific Cohort Provided the ID
     function getCohortGraduates($id){
         $graduates = Students::where('cohort', $id)->get();
         return response()->json(['status' => 200, 'graduates' => $graduates, 'noOfGraduates' => $graduates->count()]);
